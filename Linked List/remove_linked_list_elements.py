a663353b839587af8caa5aa49bda6c59b18f821e
Rahul Jadhav <rj2412003@gmail.com>
2026-02-19 23:50:17 +0530

    Solved LeetCode Remove Linked List Elements (In-place Deletion)

---
+++ Linked List/remove_linked_list_elements.py
@@ -0,0 +1,40 @@
+# Problem Name: Remove Linked List Elements
+# Platform: LeetCode
+# Topic: Linked List
+# Difficulty: Easy
+#
+# Approach:
+# - Create a dummy node pointing to head
+# - Traverse using a pointer starting from dummy
+# - If next node value equals target, skip it
+# - Otherwise move forward
+# - Return dummy.next as the new head
+#
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: Optional[ListNode]
+        :type val: int
+        :rtype: Optional[ListNode]
+        """
+
+        dummy = ListNode(0)
+        dummy.next = head
+        cur = dummy
+
+        while cur.next:
+            if cur.next.val == val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+
+        return dummy.next
